5a23753fea5e723b4b4ab00902ccc1fe49a9be67
Jakob Bang Helvind <jbh@e-conomic.com>
2018-09-12 16:43:08 +0200

    Name change machine learning service

--- gen.sh
+++ gen.sh
@@ -3,4 +3,4 @@
 # protoc -I imgsvc/ -I ssn/imgsvc.proto  --go_out=plugins=grpc:imgsvc/
 protoc --proto_path=. ssn/*.proto  --go_out=plugins=grpc,paths=source_relative:.
 protoc --proto_path=. imageservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.
-protoc --proto_path=. showmethemoney/*.proto  --go_out=plugins=grpc,paths=source_relative:.
+protoc --proto_path=. ovlservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.
