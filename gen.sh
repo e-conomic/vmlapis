c6b0d41ed15c2510cc717b0c11327e49edbe1c40
Jakob Bang Helvind <jbh@e-conomic.com>
2018-09-12 16:17:41 +0200

    Fix Go package prefixing and compiling

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 # protoc -I imgsvc/ -I ssn/imgsvc.proto  --go_out=plugins=grpc:imgsvc/
-protoc -I ssn/ -I imageservice/ ssn/*.proto  --go_out=plugins=grpc:ssn/
-protoc -I imageservice/ imageservice/*.proto  --go_out=plugins=grpc:imageservice/
-protoc -I showmethemoney/ showmethemoney/*.proto  --go_out=plugins=grpc:showmethemoney/
+protoc --proto_path=. ssn/*.proto  --go_out=plugins=grpc,paths=source_relative:.
+protoc --proto_path=. imageservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.
+protoc --proto_path=. showmethemoney/*.proto  --go_out=plugins=grpc,paths=source_relative:.
