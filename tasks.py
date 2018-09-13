87138642c979efa4d679e3f3925761ef39fb955e
JacobSMoller <scherffenberg91@gmail.com>
2018-09-13 16:20:07 +0200

    Add go pb files.

--- tasks.py
+++ tasks.py
@@ -4,17 +4,15 @@ from invoke import task
 
 @task
 def build_py(ctx):
-    ctx.run('python3 -m grpc_tools.protoc -I . --python_out=py --grpc_python_out=py ovlservice/ovlservice.proto')
-    ctx.run('python3 -m grpc_tools.protoc -I . --python_out=py --grpc_python_out=py imageservice/imageservice.proto')
-    ctx.run('python3 -m grpc_tools.protoc -I ./imageservice -I ./ovlservice -I . --python_out=py --grpc_python_out=py ssn/ssn.proto')
-    ctx.run('touch ./py/imageservice/__init__.py')
-    ctx.run('touch ./py/ovlservice/__init__.py')
-    ctx.run('touch ./py/ssn/__init__.py')
+    ctx.run('python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. ovlservice/ovlservice.proto')
+    ctx.run('python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. imageservice/imageservice.proto')
+    ctx.run('python3 -m grpc_tools.protoc -I ./imageservice -I ./ovlservice -I . --python_out=. --grpc_python_out=. ssn/ssn.proto')
+    ctx.run('touch ./ssn/__init__.py')
     ctx.run('python3 setup.py bdist_wheel')
 
 
 @task
 def build_go(ctx):
     ctx.run('protoc --proto_path=. ssn/*.proto  --go_out=plugins=grpc,paths=source_relative:.')
-    ctx.run('protoc --proto_path=. imageservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.')
-    ctx.run('protoc --proto_path=. ovlservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.')
+    ctx.run('protoc --proto_path=. ssn/imageservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.')
+    ctx.run('protoc --proto_path=. ssn/mlservice/*.proto  --go_out=plugins=grpc,paths=source_relative:.')
