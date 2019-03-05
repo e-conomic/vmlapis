0af02d8c503abaee2081d04eaa7f902fb2c2c3cc
JacobSMoller <scherffenberg91@gmail.com>
2019-03-05 13:22:01 +0100

    Use travis tags

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ import os
 import datetime
 
 
-date = datetime.datetime.now().strf('%Y%m%d')
+tag = os.environ.get('TRAVIS_TAG')
+assert tag
+version = tag.strip('v')
 
 setuptools.setup(
     name="ssn_proto",
-    version='{}-{}'.format(date, os.environ.get('TRAVIS_BUILD_NUMBER', 0)),
+    version=version,
     description="A package containing ssn protobuf definitions for python",
     url="https://github.com/economic/vmlapis",
     packages=setuptools.find_packages(where='gen/python/'),
