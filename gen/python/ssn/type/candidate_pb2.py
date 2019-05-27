f7318457d69b8cf14f689945bf9df53d9b69455d
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2019-05-27 16:07:23 +0200

    Added reference to page where candidate was found (#45)

--- gen/python/ssn/type/candidate_pb2.py
+++ gen/python/ssn/type/candidate_pb2.py
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='ssn.type',
   syntax='proto3',
   serialized_options=_b('Z\004type'),
-  serialized_pb=_b('\n\x18ssn/type/candidate.proto\x12\x08ssn.type\x1a\x1egoogle/protobuf/wrappers.proto\x1a\x17ssn/type/geometry.proto\"\xb2\x01\n\nConfidence\x12)\n\x05level\x18\x01 \x01(\x0e\x32\x1a.ssn.type.Confidence.Level\x12*\n\x05value\x18\x02 \x01(\x0b\x32\x1b.google.protobuf.FloatValue\"M\n\x05Level\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0c\n\x08VERY_LOW\x10\x01\x12\x07\n\x03LOW\x10\x02\x12\x07\n\x03MID\x10\x03\x12\x08\n\x04HIGH\x10\x04\x12\r\n\tVERY_HIGH\x10\x05\"\x80\x01\n\tCandidate\x12\r\n\x05value\x18\x01 \x01(\t\x12\x0c\n\x04text\x18\x02 \x01(\t\x12(\n\nconfidence\x18\x03 \x01(\x0b\x32\x14.ssn.type.Confidence\x12,\n\x0c\x62ounding_box\x18\x04 \x01(\x0b\x32\x16.ssn.type.BoundingPolyB\x06Z\x04typeb\x06proto3')
+  serialized_pb=_b('\n\x18ssn/type/candidate.proto\x12\x08ssn.type\x1a\x1egoogle/protobuf/wrappers.proto\x1a\x17ssn/type/geometry.proto\"\xb2\x01\n\nConfidence\x12)\n\x05level\x18\x01 \x01(\x0e\x32\x1a.ssn.type.Confidence.Level\x12*\n\x05value\x18\x02 \x01(\x0b\x32\x1b.google.protobuf.FloatValue\"M\n\x05Level\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0c\n\x08VERY_LOW\x10\x01\x12\x07\n\x03LOW\x10\x02\x12\x07\n\x03MID\x10\x03\x12\x08\n\x04HIGH\x10\x04\x12\r\n\tVERY_HIGH\x10\x05\"\xe8\x01\n\tCandidate\x12\r\n\x05value\x18\x01 \x01(\t\x12\x0c\n\x04text\x18\x02 \x01(\t\x12(\n\nconfidence\x18\x03 \x01(\x0b\x32\x14.ssn.type.Confidence\x12,\n\x0c\x62ounding_box\x18\x04 \x01(\x0b\x32\x16.ssn.type.BoundingPoly\x12&\n\x04type\x18\x05 \x01(\x0e\x32\x18.ssn.type.Candidate.Type\x12\x10\n\x08page_ref\x18\x06 \x01(\r\",\n\x04Type\x12\x0b\n\x07UNKNOWN\x10\x00\x12\t\n\x05\x46IELD\x10\x01\x12\x0c\n\x08PROPERTY\x10\x02\x42\x06Z\x04typeb\x06proto3')
   ,
   dependencies=[google_dot_protobuf_dot_wrappers__pb2.DESCRIPTOR,ssn_dot_type_dot_geometry__pb2.DESCRIPTOR,])
 
@@ -64,6 +64,32 @@ _CONFIDENCE_LEVEL = _descriptor.EnumDescriptor(
   serialized_end=274,
 )
 _sym_db.RegisterEnumDescriptor(_CONFIDENCE_LEVEL)
+
+_CANDIDATE_TYPE = _descriptor.EnumDescriptor(
+  name='Type',
+  full_name='ssn.type.Candidate.Type',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='UNKNOWN', index=0, number=0,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='FIELD', index=1, number=1,
+      serialized_options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='PROPERTY', index=2, number=2,
+      serialized_options=None,
+      type=None),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=465,
+  serialized_end=509,
+)
+_sym_db.RegisterEnumDescriptor(_CANDIDATE_TYPE)
 
 
 _CONFIDENCE = _descriptor.Descriptor(
@@ -140,11 +166,26 @@ _CANDIDATE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='type', full_name='ssn.type.Candidate.type', index=4,
+      number=5, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='page_ref', full_name='ssn.type.Candidate.page_ref', index=5,
+      number=6, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
   nested_types=[],
   enum_types=[
+    _CANDIDATE_TYPE,
   ],
   serialized_options=None,
   is_extendable=False,
@@ -153,7 +194,7 @@ _CANDIDATE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=277,
-  serialized_end=405,
+  serialized_end=509,
 )
 
 _CONFIDENCE.fields_by_name['level'].enum_type = _CONFIDENCE_LEVEL
@@ -161,6 +202,8 @@ _CONFIDENCE.fields_by_name['value'].message_type = google_dot_protobuf_dot_wrapp
 _CONFIDENCE_LEVEL.containing_type = _CONFIDENCE
 _CANDIDATE.fields_by_name['confidence'].message_type = _CONFIDENCE
 _CANDIDATE.fields_by_name['bounding_box'].message_type = ssn_dot_type_dot_geometry__pb2._BOUNDINGPOLY
+_CANDIDATE.fields_by_name['type'].enum_type = _CANDIDATE_TYPE
+_CANDIDATE_TYPE.containing_type = _CANDIDATE
 DESCRIPTOR.message_types_by_name['Confidence'] = _CONFIDENCE
 DESCRIPTOR.message_types_by_name['Candidate'] = _CANDIDATE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
