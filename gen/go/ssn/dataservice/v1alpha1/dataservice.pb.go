5952e5404303194eab5bf1468652c3e97ec71863
JacobSMoller <scherffenberg91@gmail.com>
2019-09-12 10:55:56 +0200

    Remove weird import in generated dataservice

--- gen/go/ssn/dataservice/v1alpha1/dataservice.pb.go
+++ gen/go/ssn/dataservice/v1alpha1/dataservice.pb.go
@@ -6,7 +6,6 @@ package dataservice
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
-import _ "."
 import _type "github.com/e-conomic/vmlapis/gen/go/ssn/type"
 import empty "github.com/golang/protobuf/ptypes/empty"
 import wrappers "github.com/golang/protobuf/ptypes/wrappers"
