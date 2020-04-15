accc1b1b4abaa11f3f0f4b5ce062a9c5c9903bb8
Lars Schöning <lars.schoning@visma.com>
2020-04-15 14:46:45 +0200

    Update gen

---
+++ gen/js/asgt/dataservice/v1alpha1/bank_data_service_grpc_web_pb.js
@@ -0,0 +1,324 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for asgt.dataservice.v1alpha1
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+
+const grpc = {};
+grpc.web = require('grpc-web');
+
+
+var asgt_dataservice_v1alpha1_delete_pb = require('../../../asgt/dataservice/v1alpha1/delete_pb.js')
+
+var asgt_type_bank_transaction_pb = require('../../../asgt/type/bank_transaction_pb.js')
+
+var asgt_type_retention_policy_pb = require('../../../asgt/type/retention_policy_pb.js')
+
+var asgt_type_target_value_pb = require('../../../asgt/type/target_value_pb.js')
+
+var google_api_annotations_pb = require('../../../google/api/annotations_pb.js')
+
+var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js')
+const proto = {};
+proto.asgt = {};
+proto.asgt.dataservice = {};
+proto.asgt.dataservice.v1alpha1 = require('./bank_data_service_pb.js');
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServiceClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+};
+
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServicePromiseClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest,
+ *   !proto.google.protobuf.Empty>}
+ */
+const methodDescriptor_BankDataService_CreateBankDataset = new grpc.web.MethodDescriptor(
+  '/asgt.dataservice.v1alpha1.BankDataService/CreateBankDataset',
+  grpc.web.MethodType.UNARY,
+  proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest,
+  google_protobuf_empty_pb.Empty,
+  /**
+   * @param {!proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_empty_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest,
+ *   !proto.google.protobuf.Empty>}
+ */
+const methodInfo_BankDataService_CreateBankDataset = new grpc.web.AbstractClientBase.MethodInfo(
+  google_protobuf_empty_pb.Empty,
+  /**
+   * @param {!proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_empty_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.google.protobuf.Empty)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.google.protobuf.Empty>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServiceClient.prototype.createBankDataset =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/asgt.dataservice.v1alpha1.BankDataService/CreateBankDataset',
+      request,
+      metadata || {},
+      methodDescriptor_BankDataService_CreateBankDataset,
+      callback);
+};
+
+
+/**
+ * @param {!proto.asgt.dataservice.v1alpha1.CreateBankDatasetRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.google.protobuf.Empty>}
+ *     A native promise that resolves to the response
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServicePromiseClient.prototype.createBankDataset =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/asgt.dataservice.v1alpha1.BankDataService/CreateBankDataset',
+      request,
+      metadata || {},
+      methodDescriptor_BankDataService_CreateBankDataset);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.asgt.dataservice.v1alpha1.AppendBankDataRequest,
+ *   !proto.google.protobuf.Empty>}
+ */
+const methodDescriptor_BankDataService_AppendBankData = new grpc.web.MethodDescriptor(
+  '/asgt.dataservice.v1alpha1.BankDataService/AppendBankData',
+  grpc.web.MethodType.UNARY,
+  proto.asgt.dataservice.v1alpha1.AppendBankDataRequest,
+  google_protobuf_empty_pb.Empty,
+  /**
+   * @param {!proto.asgt.dataservice.v1alpha1.AppendBankDataRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_empty_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.asgt.dataservice.v1alpha1.AppendBankDataRequest,
+ *   !proto.google.protobuf.Empty>}
+ */
+const methodInfo_BankDataService_AppendBankData = new grpc.web.AbstractClientBase.MethodInfo(
+  google_protobuf_empty_pb.Empty,
+  /**
+   * @param {!proto.asgt.dataservice.v1alpha1.AppendBankDataRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_empty_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.asgt.dataservice.v1alpha1.AppendBankDataRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.google.protobuf.Empty)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.google.protobuf.Empty>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServiceClient.prototype.appendBankData =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/asgt.dataservice.v1alpha1.BankDataService/AppendBankData',
+      request,
+      metadata || {},
+      methodDescriptor_BankDataService_AppendBankData,
+      callback);
+};
+
+
+/**
+ * @param {!proto.asgt.dataservice.v1alpha1.AppendBankDataRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.google.protobuf.Empty>}
+ *     A native promise that resolves to the response
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServicePromiseClient.prototype.appendBankData =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/asgt.dataservice.v1alpha1.BankDataService/AppendBankData',
+      request,
+      metadata || {},
+      methodDescriptor_BankDataService_AppendBankData);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.asgt.dataservice.v1alpha1.DeleteRequest,
+ *   !proto.google.protobuf.Empty>}
+ */
+const methodDescriptor_BankDataService_DeleteBankData = new grpc.web.MethodDescriptor(
+  '/asgt.dataservice.v1alpha1.BankDataService/DeleteBankData',
+  grpc.web.MethodType.UNARY,
+  asgt_dataservice_v1alpha1_delete_pb.DeleteRequest,
+  google_protobuf_empty_pb.Empty,
+  /**
+   * @param {!proto.asgt.dataservice.v1alpha1.DeleteRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_empty_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.asgt.dataservice.v1alpha1.DeleteRequest,
+ *   !proto.google.protobuf.Empty>}
+ */
+const methodInfo_BankDataService_DeleteBankData = new grpc.web.AbstractClientBase.MethodInfo(
+  google_protobuf_empty_pb.Empty,
+  /**
+   * @param {!proto.asgt.dataservice.v1alpha1.DeleteRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_empty_pb.Empty.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.asgt.dataservice.v1alpha1.DeleteRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.google.protobuf.Empty)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.google.protobuf.Empty>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServiceClient.prototype.deleteBankData =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/asgt.dataservice.v1alpha1.BankDataService/DeleteBankData',
+      request,
+      metadata || {},
+      methodDescriptor_BankDataService_DeleteBankData,
+      callback);
+};
+
+
+/**
+ * @param {!proto.asgt.dataservice.v1alpha1.DeleteRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.google.protobuf.Empty>}
+ *     A native promise that resolves to the response
+ */
+proto.asgt.dataservice.v1alpha1.BankDataServicePromiseClient.prototype.deleteBankData =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/asgt.dataservice.v1alpha1.BankDataService/DeleteBankData',
+      request,
+      metadata || {},
+      methodDescriptor_BankDataService_DeleteBankData);
+};
+
+
+module.exports = proto.asgt.dataservice.v1alpha1;
+
