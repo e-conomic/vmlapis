ae6254e985919333d630bd19b1ae94356b704b17
Michal Derdak <michalderdak@gmail.com>
2020-11-27 12:35:34 +0100

    gen validation only for what's necessary

--- scripts/go-gen-asgt.sh
+++ scripts/go-gen-asgt.sh
@@ -24,9 +24,8 @@ protoc -I./deps/googleapis -I./deps/validate -I./deps/tensorflow -I./proto \
 		--go-grpc_opt=paths=source_relative \
 		proto/asgt/modelregistry/v1/*.proto
 
-protoc -I./deps/googleapis -I./deps/validate -I./deps/tensorflow -I./proto \
+protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
-		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
