39c0e5fd83c53c474622b78f93a6d284cc9f550e
Michal Derdak <michalderdak@gmail.com>
2020-11-27 13:41:45 +0100

    rules for creating ds in asgt

--- scripts/go-gen-asgt.sh
+++ scripts/go-gen-asgt.sh
@@ -8,17 +8,15 @@ protoc -I./deps/googleapis -I./deps/validate -I./deps/tensorflow -I./proto \
 		--go-grpc_opt=paths=source_relative \
 		proto/asgt/data/v1/*.proto
 
-protoc -I./deps/googleapis -I./deps/validate -I./deps/tensorflow -I./proto \
+protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
-		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
 		proto/asgt/jester/v1/*.proto
 
-protoc -I./deps/googleapis -I./deps/validate -I./deps/tensorflow -I./proto \
+protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
-		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
