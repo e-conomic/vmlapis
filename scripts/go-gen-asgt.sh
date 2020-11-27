f4e53177597c64f1182793817d1b00ded851dda5
JacobSMoller <scherffenberg91@gmail.com>
2020-11-27 12:11:04 +0100

    relative output

--- scripts/go-gen-asgt.sh
+++ scripts/go-gen-asgt.sh
@@ -2,14 +2,15 @@
 
 protoc -I./deps/googleapis -I./deps/validate -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
+		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
-  		--validate_out=lang=go:gen/go \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
 		proto/asgt/data/v1/*.proto
 
 protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
+		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
@@ -17,6 +18,7 @@ protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 
 protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
+		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
@@ -24,6 +26,7 @@ protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 
 protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
 		--go_out=gen/go/ \
+		--validate_out=lang=go,paths=source_relative:gen/go \
 		--go-grpc_out=gen/go/ \
 		--go_opt=paths=source_relative \
 		--go-grpc_opt=paths=source_relative \
