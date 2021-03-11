e969c87c88bc54935899038e1c56a571db7d5ab1
Lars Schöning <lars.schoning@visma.com>
2021-03-11 13:32:07 +0100

    Add tier (2)

---
+++ gen/js/ssn/type/tier_pb.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+var jspb = require('google-protobuf');
+var goog = jspb;
+var global = Function('return this')();
+
+goog.exportSymbol('proto.ssn.type.Tier', null, global);
+/**
+ * @enum {number}
+ */
+proto.ssn.type.Tier = {
+  DEFAULT: 0,
+  STANDARD: 1,
+  PREMIUM: 2
+};
+
+goog.object.extend(exports, proto.ssn.type);
