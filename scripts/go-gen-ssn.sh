0c951dbca2d6e24c3a81498df138199e8031d6d2
Lars Schöning <lars.schoning@visma.com>
2021-06-29 09:38:07 +0200

    Add SSN dataservice to docs

--- scripts/go-gen-ssn.sh
+++ scripts/go-gen-ssn.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 protoc -I./deps/googleapis -I./deps/tensorflow -I./deps/protoc-gen-openapiv2 -I./proto \
-    --openapiv2_out gen/openapiv2/ \
-    --openapiv2_opt logtostderr=true \
-    --openapiv2_opt allow_merge=true \
-    --openapiv2_opt merge_file_name=ssn/v1.json \
-		--go_out=gen/go/ \
-		--go-grpc_out=gen/go/ \
-		--go_opt=paths=source_relative \
-		--go-grpc_opt=paths=source_relative \
-		proto/ssn/annotator/v1/annotator.proto
+  --openapiv2_out gen/openapiv2/ \
+  --openapiv2_opt logtostderr=true \
+  --openapiv2_opt allow_merge=true \
+  --openapiv2_opt merge_file_name=ssn/v1.json \
+  --go_out=gen/go/ \
+  --go-grpc_out=gen/go/ \
+  --go_opt=paths=source_relative \
+  --go-grpc_opt=paths=source_relative \
+  proto/ssn/annotator/v1/annotator.proto \
+  proto/ssn/dataservice/v1/*.proto
 
 protoc -I./deps/googleapis -I./deps/tensorflow -I./deps/protoc-gen-openapiv2 -I./proto \
-		--go_out=gen/go/ \
-		--go-grpc_out=gen/go/ \
-		--go_opt=paths=source_relative \
-		--go-grpc_opt=paths=source_relative \
-		proto/ssn/annotator/v1/annotator.proto
+  --go_out=gen/go/ \
+  --go-grpc_out=gen/go/ \
+  --go_opt=paths=source_relative \
+  --go-grpc_opt=paths=source_relative \
+  proto/ssn/annotator/v1/annotator.proto
 
 protoc -I./deps/googleapis -I./deps/tensorflow -I./proto \
   --go_out=gen/go/ \
