eae6dac5745545c09d00ca9e55a88ee65687b3ff
MiroJ <miroslav.jascur@visma.com>
2022-11-03 12:33:16 +0100

    Add cv scanning API

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Protobuf API definitions for VML
-
+### Install buff
+To install buff on OSX 
+```
+brew install bufbuild/buf/buf 
+```
 ### Generate
 
 Generation is now done with a dockerfile
@@ -11,3 +15,5 @@ make
 The files in the `proto/` directory are automatically formatted by the [Buf's linter](https://docs.buf.build/lint/overview).
 
 Code is checked for breaking changes against the `master` branch. If generation fails at the `buf breaking` command during the `make` process, follow the instructions from the output to make the new code backwards-compatible.
+
+
--- gen/js/cv/scanner/v1/scanner_grpc_web_pb.js
+++ gen/js/cv/scanner/v1/scanner_grpc_web_pb.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview gRPC-Web generated client stub for ssn.annotator.v1
+ * @fileoverview gRPC-Web generated client stub for cv.scanner.v1
  * @enhanceable
  * @public
  */
@@ -8,7 +8,7 @@
 // versions:
 // 	protoc-gen-grpc-web v1.4.2
 // 	protoc              v0.0.0
-// source: ssn/annotator/v1/annotator.proto
+// source: cv/scanner/v1/scanner.proto
 
 
 /* eslint-disable */
@@ -22,17 +22,13 @@ grpc.web = require('grpc-web');
 
 var google_api_annotations_pb = require('../../../google/api/annotations_pb.js')
 
-var protoc$gen$openapiv2_options_annotations_pb = require('../../../protoc-gen-openapiv2/options/annotations_pb.js')
+var google_api_http_pb = require('../../../google/api/http_pb.js')
 
 var ssn_type_candidate_pb = require('../../../ssn/type/candidate_pb.js')
-
-var ssn_type_text_annotation_pb = require('../../../ssn/type/text_annotation_pb.js')
-
-var ssn_type_tier_pb = require('../../../ssn/type/tier_pb.js')
 const proto = {};
-proto.ssn = {};
-proto.ssn.annotator = {};
-proto.ssn.annotator.v1 = require('./annotator_pb.js');
+proto.cv = {};
+proto.cv.scanner = {};
+proto.cv.scanner.v1 = require('./scanner_pb.js');
 
 /**
  * @param {string} hostname
@@ -42,7 +38,7 @@ proto.ssn.annotator.v1 = require('./annotator_pb.js');
  * @struct
  * @final
  */
-proto.ssn.annotator.v1.DocumentAnnotatorClient =
+proto.cv.scanner.v1.CvScannerClient =
     function(hostname, credentials, options) {
   if (!options) options = {};
   options.format = 'text';
@@ -68,7 +64,7 @@ proto.ssn.annotator.v1.DocumentAnnotatorClient =
  * @struct
  * @final
  */
-proto.ssn.annotator.v1.DocumentAnnotatorPromiseClient =
+proto.cv.scanner.v1.CvScannerPromiseClient =
     function(hostname, credentials, options) {
   if (!options) options = {};
   options.format = 'text';
@@ -89,63 +85,63 @@ proto.ssn.annotator.v1.DocumentAnnotatorPromiseClient =
 /**
  * @const
  * @type {!grpc.web.MethodDescriptor<
- *   !proto.ssn.annotator.v1.DocumentAnnotatorRequest,
- *   !proto.ssn.annotator.v1.DocumentAnnotatorResponse>}
+ *   !proto.cv.scanner.v1.ScanCVRequest,
+ *   !proto.cv.scanner.v1.ScanCVResponse>}
  */
-const methodDescriptor_DocumentAnnotator_AnnotateDocument = new grpc.web.MethodDescriptor(
-  '/ssn.annotator.v1.DocumentAnnotator/AnnotateDocument',
+const methodDescriptor_CvScanner_ScanCV = new grpc.web.MethodDescriptor(
+  '/cv.scanner.v1.CvScanner/ScanCV',
   grpc.web.MethodType.UNARY,
-  proto.ssn.annotator.v1.DocumentAnnotatorRequest,
-  proto.ssn.annotator.v1.DocumentAnnotatorResponse,
+  proto.cv.scanner.v1.ScanCVRequest,
+  proto.cv.scanner.v1.ScanCVResponse,
   /**
-   * @param {!proto.ssn.annotator.v1.DocumentAnnotatorRequest} request
+   * @param {!proto.cv.scanner.v1.ScanCVRequest} request
    * @return {!Uint8Array}
    */
   function(request) {
     return request.serializeBinary();
   },
-  proto.ssn.annotator.v1.DocumentAnnotatorResponse.deserializeBinary
+  proto.cv.scanner.v1.ScanCVResponse.deserializeBinary
 );
 
 
 /**
- * @param {!proto.ssn.annotator.v1.DocumentAnnotatorRequest} request The
+ * @param {!proto.cv.scanner.v1.ScanCVRequest} request The
  *     request proto
  * @param {?Object<string, string>} metadata User defined
  *     call metadata
- * @param {function(?grpc.web.RpcError, ?proto.ssn.annotator.v1.DocumentAnnotatorResponse)}
+ * @param {function(?grpc.web.RpcError, ?proto.cv.scanner.v1.ScanCVResponse)}
  *     callback The callback function(error, response)
- * @return {!grpc.web.ClientReadableStream<!proto.ssn.annotator.v1.DocumentAnnotatorResponse>|undefined}
+ * @return {!grpc.web.ClientReadableStream<!proto.cv.scanner.v1.ScanCVResponse>|undefined}
  *     The XHR Node Readable Stream
  */
-proto.ssn.annotator.v1.DocumentAnnotatorClient.prototype.annotateDocument =
+proto.cv.scanner.v1.CvScannerClient.prototype.scanCV =
     function(request, metadata, callback) {
   return this.client_.rpcCall(this.hostname_ +
-      '/ssn.annotator.v1.DocumentAnnotator/AnnotateDocument',
+      '/cv.scanner.v1.CvScanner/ScanCV',
       request,
       metadata || {},
-      methodDescriptor_DocumentAnnotator_AnnotateDocument,
+      methodDescriptor_CvScanner_ScanCV,
       callback);
 };
 
 
 /**
- * @param {!proto.ssn.annotator.v1.DocumentAnnotatorRequest} request The
+ * @param {!proto.cv.scanner.v1.ScanCVRequest} request The
  *     request proto
  * @param {?Object<string, string>=} metadata User defined
  *     call metadata
- * @return {!Promise<!proto.ssn.annotator.v1.DocumentAnnotatorResponse>}
+ * @return {!Promise<!proto.cv.scanner.v1.ScanCVResponse>}
  *     Promise that resolves to the response
  */
-proto.ssn.annotator.v1.DocumentAnnotatorPromiseClient.prototype.annotateDocument =
+proto.cv.scanner.v1.CvScannerPromiseClient.prototype.scanCV =
     function(request, metadata) {
   return this.client_.unaryCall(this.hostname_ +
-      '/ssn.annotator.v1.DocumentAnnotator/AnnotateDocument',
+      '/cv.scanner.v1.CvScanner/ScanCV',
       request,
       metadata || {},
-      methodDescriptor_DocumentAnnotator_AnnotateDocument);
+      methodDescriptor_CvScanner_ScanCV);
 };
 
 
-module.exports = proto.ssn.annotator.v1;
+module.exports = proto.cv.scanner.v1;
 
