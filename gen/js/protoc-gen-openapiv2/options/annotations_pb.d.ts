06c3ffd5be586ced8e8de8fbf4490ed46fd2434f
MiroJ <miroslav.jascur@visma.com>
2024-01-03 11:15:20 +0100

    add line total and line unit price

--- gen/js/protoc-gen-openapiv2/options/annotations_pb.d.ts
+++
@@ -1,6 +0,0 @@
-import * as jspb from 'google-protobuf'
-
-import * as google_protobuf_descriptor_pb from 'google-protobuf/google/protobuf/descriptor_pb';
-import * as protoc$gen$openapiv2_options_openapiv2_pb from '../../protoc-gen-openapiv2/options/openapiv2_pb';
-
-
